38fb0deabc7fe555c39151cb82b4ecc6a69fd7cb
jessemiel <jessemiel@gmail.com>
2020-05-25 23:41:20 +0800

    new files
    gitignore
    util class
    and app center secret key

---
+++ Vibratr/Vibratr/Util.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Vibratr
+{
+    public class Util
+    {
+        //Secret key for android app center
+        public const string AppCenterKeyAndroid = "android=17a4d54f-0d45-48c1-af51-77357f0cc9e6;";
+    }
+}
